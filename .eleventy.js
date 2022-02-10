19f816b857e2f0bd7b8ccd875744c34c6a95aac0
Joshua Bartz <jsh@brtz.me>
2022-02-10 12:46:11 -0500

    Nuking duplicate files
    This happens every time I merge to main. sup with that?

--- .eleventy.js
+++ .eleventy.js
@@ -1,31 +1,29 @@
 //== Eleventy Config File ==//
 
 module.exports = function (eleventyConfig) {
-  // Add passthrough directories & files
-  eleventyConfig.addPassthroughCopy("./src/style/");
-  eleventyConfig.addPassthroughCopy("./src/img/");
-
-  /* Markdown-It 'markdownify' filter
-	source: BradCoffield/kidlitconnection@e42a6de)
-  */
-  const md = require("markdown-it")({
-	html: true,
-	linkify: true,
-	typographer: true,
-  });
-
-  eleventyConfig.addFilter("markdownify", (markdownString) =>
+	// Add passthrough directories & files
+	eleventyConfig.addPassthroughCopy("./src/style/");
+	eleventyConfig.addPassthroughCopy("./src/img/");
+	
+	// Markdown-It 'markdownify' filter source: BradCoffield/kidlitconnection@e42a6de)
+	const md = require("markdown-it")({
+		html: true,
+		linkify: true,
+		typographer: true,
+	});
+	
+	eleventyConfig.addFilter("markdownify", (markdownString) =>
 	md.render(markdownString)
-  );
-
-  // Manual passthrough template extensions
-  return {
-	dir: {
-	  input: "src",
-	  output: "dist",
-	},
-	markdownTemplateEngine: "njk",
-	dataTemplateEngine: "njk",
-	htmlTemplateEngine: "njk",
-  };
+	);
+	
+	// Manual passthrough template extensions
+	return {
+		dir: {
+  			input: "src",
+  			output: "dist",
+		},
+		markdownTemplateEngine: "njk",
+		dataTemplateEngine: "njk",
+		htmlTemplateEngine: "njk",
+	};
 };
