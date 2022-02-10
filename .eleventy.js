b0c96864b5d34f0df69b80fe4c941a3b9ab62d50
Joshua Bartz <jsh@brtz.me>
2022-02-10 17:43:31 -0500

    dev and prod favicons
    it works locally...let's see if it works on Netlify.

--- .eleventy.js
+++ .eleventy.js
@@ -2,9 +2,19 @@
 
 module.exports = function (eleventyConfig) {
 	// Add passthrough directories & files
+	
+	let env = process.env.ELEVENTY_ENV;
+	
+		if (env === "prod") {
+			eleventyConfig.addPassthroughCopy({ "./src/img/favicons_prod": "/" });
+		}
+		else if (env === "dev") {
+			eleventyConfig.addPassthroughCopy({ "./src/img/favicons_dev": "/" });
+		}
+		
 	eleventyConfig.addPassthroughCopy("./src/style/");
 	eleventyConfig.addPassthroughCopy("./src/img/");
-	eleventyConfig.addPassthroughCopy({ "./src/img/favicon": "/" });
+	// eleventyConfig.addPassthroughCopy({ "./src/img/favicon": "/" });
 	
 	// Markdown-It 'markdownify' filter source: BradCoffield/kidlitconnection@e42a6de)
 	const md = require("markdown-it")({
