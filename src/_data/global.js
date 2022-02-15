2b2bed76cf0da2ca851da7c5b590b41a52a612f0
Joshua Bartz <jsh@brtz.me>
2022-02-15 13:10:01 -0500

    updated gitignore to get rid of .nova
    cleaning up some markup.

--- src/_data/global.js
+++ src/_data/global.js
@@ -1,8 +1,10 @@
+//== Global Declarations File ==//
+
 module.exports = {
-  random() {
-	const segment = () => {
-	  return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
-	};
-	return `${segment()}-${segment()}-${segment()}`;
-  }
+	random() {
+		const segment = () => {
+			return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+			};
+			return `${segment()}-${segment()}-${segment()}`;
+	}
 };
